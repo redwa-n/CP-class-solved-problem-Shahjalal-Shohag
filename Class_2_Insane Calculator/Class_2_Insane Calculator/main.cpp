f57c26a622be3e11fd35f7f746d746bfc5c4295a
Kazi Redwan <redwan.stu@gmail.com>
2024-02-29 01:58:17 +0600

    Add files via upload

---
+++ Class_2_Insane Calculator/Class_2_Insane Calculator/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,c,d,result;
+    cin>>a>>b;
+    c=a&b;
+    d=a|b;
+    result=(c^d);
+    cout<<result;
+    //144152 176217//33601
+}
